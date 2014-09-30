bb220b096548e4d946d05a9f7b2e0b3b1cf37294
Matthew Pineau <matt.w.pineau@gmail.com>
2014-09-30 13:21:28 -0400

    Author: Matt Pineau Date: 9/30/14
    Commit lab4.sql for first time.
    Nearly complete besides number 4, which will be fixed in a later commit.

---
+++ lab4.sql
@@ -0,0 +1,38 @@
+--Author: Matt Pineau
+--Date: 9/26/14
+--Class: Database Management
+--Assignment: Lab 4
+
+--1--
+select city
+from agent
+where aid in (select aid
+              from orders
+              where cid in (select cid
+                            from customers
+                            where name = 'TipTop'));
+                            
+--2--
+select pid
+from products
+where pid in (select pid
+             from orders
+             where aid in (select aid
+                          from orders
+                          where cid in (select cid
+                                       from customers
+                                       where city = 'Kyoto')));
+
+--3--
+select cid, name
+from customers
+where cid in (select cid
+             from orders
+             where aid != 'a04');
+
+--4***********--
+select cid, name
+from customers
+where cid in (select cid
+             from orders
+             where pid = ('p01', 'p07'));
